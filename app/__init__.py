020e46a162285583c7eff211e24e00f103fcb338
Amr Abouelmagd <amrakmal97@gmail.com>
2024-06-21 14:00:41 -0500

    rebasing and fixing conflict

--- app/__init__.py
+++ app/__init__.py
@@ -8,13 +8,13 @@ from jinja2 import Environment, FileSystemLoader
 load_dotenv()
 app = Flask(__name__)
 
-hobbies = ["Football", "Video Games", "Watching Aurora"]
+hobbies_arr = ["Football", "Video Games", "Watching Aurora"]
 work_experience = ["SWE @ Incorta     2021-2024", "SWE Intern @ M3ntorship      01/2021-04/2021"]
 education = ["M.Sc. @ TTU     2024-2026", "B.Sc. @ Alexandria University    2026-2021"]
 
 @app.route('/', endpoint='index')
 def index():
-    return render_template('index.html', education=education, work_experience=work_experience, hobbies=hobbies, title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', education=education, work_experience=work_experience, hobbies=hobbies_arr, title="MLH Fellow", url=os.getenv("URL"))
 
 @app.route('/hobbies', endpoint='hobbies')
 def hobbies():
