8349c6ee3f3f10e24f5f97a4fac87350dbdf43a5
Amr Abouelmagd <amrakmal97@gmail.com>
2024-06-21 15:47:00 -0500

    Issue #5

--- app/utils.py
+++ app/utils.py
@@ -8,7 +8,11 @@ class Project:
 
 class User:
     def __init__(self) -> None:  
-        self.hobbies = ["Football", "Video Games", "Watching Aurora"]
+        self.hobbies = [
+            {"name": "Football", "img_code": "fb"},
+            {"name": "Video Games", "img_code": "vg"},
+            {"name": "Watching Aurora", "img_code": "au"}
+        ]
         self.work_experience = ["SWE @ Incorta     2021-2024", "SWE Intern @ M3ntorship      01/2021-04/2021"]
         self.education = ["M.Sc. @ TTU     2024-2026", "B.Sc. @ Alexandria University    2026-2021"] 
         self.projects: Project = [Project(name='Foo', desc='Foo Desc', link='https://github.com/')]
